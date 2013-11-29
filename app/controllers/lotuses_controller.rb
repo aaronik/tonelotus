3011b00999b51d732336bf14b54e30f3e329027e
Aaron Sullivan <aasullivan1618@gmail.com>
2013-11-29 15:56:20 -0800

    gabba change page to gabba

--- app/controllers/lotuses_controller.rb
+++ app/controllers/lotuses_controller.rb
@@ -1,7 +1,7 @@
 class LotusesController < ApplicationController
 	def index
 		Tracking.hit(request)
-		Gabba::Gabba.new("UA-37815249-2", "the.tonelot.us").page_view("something", "track/me")
+		Gabba::Gabba.new("UA-37815249-2", "the.tonelot.us").page_view("something", "gabba")
 
 		render :index
 	end
