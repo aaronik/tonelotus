5a0037e2b6566d42e68101c5ea67cc82aaa234cf
Aaron Sullivan <aasullivan1618@gmail.com>
2013-11-30 21:48:31 -0800

    superficial tracking fix for geocoding API's response not valid JSON

--- app/models/tracking.rb
+++ app/models/tracking.rb
@@ -2,7 +2,7 @@ class Tracking < ActiveRecord::Base
 	def self.hit(request)
 		ip = request.remote_ip
 		geo_info = request.location
-		location = "#{geo_info.city}, #{geo_info.state} #{geo_info.country_code}"
+		location = "#{geo_info.city}, #{geo_info.state} #{geo_info.country_code}" if geo_info
 		time = "#{Time.new.month} / #{Time.new.day}"
 
 		Tracking.create(ip: ip, location: location, time: time)
